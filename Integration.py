f14232dfae1136c90c53addcea486110f7259f9d
jlachterberg <j.l.achterberg@student.tudelft.nl>
2020-02-19 09:37:35 +0100

    small change

--- Integration.py
+++ Integration.py
@@ -2,4 +2,15 @@
 # x = 0
 import csv
 import math
+Nz = 81
+i = 3342
 theta_z = i - 1 / Nz * math.pi
+####
+loading = []
+with open('aerodynamicloadcrj700.dat') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    # line_count = 0
+    for row in csv_reader:
+        loading.append(row)
+    # print(f'Processed {line_count} lines.')
+print(loading)
