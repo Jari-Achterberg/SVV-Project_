88ee7eb950ba39635af90fb7b358fe1177d5a262
jlachterberg <j.l.achterberg@student.tudelft.nl>
2020-02-19 11:12:56 +0100

    added coordinate conversion

--- Integration.py
+++ Integration.py
@@ -3,8 +3,9 @@
 import csv
 import math
 Nz = 81
-i = 3342
-theta_z = i - 1 / Nz * math.pi
+Nx = 41
+Ca = 0.484  # m
+la = 1.691  # m
 ####
 loading = []
 with open('aerodynamicloadcrj700.dat') as csv_file:
@@ -14,4 +15,23 @@ with open('aerodynamicloadcrj700.dat') as csv_file:
         loading.append(row)
     # print(f'Processed {line_count} lines.')
 print(loading)
-#
+print(len(loading))
+print(len(loading[0]))
+coordinates = []
+d = dict()
+
+for j in range(1, Nx + 1):
+    theta_xi = (j - 1) / Nz * math.pi
+    theta_xi_next = j / Nz * math.pi
+    x = 0.5 * ((la / 2) * (1 - math.cos(theta_xi)) + (la/2)*(1 - math.cos(theta_xi_next)))
+    for i in range(1, Nz + 1):
+        theta_zi = (i - 1) / Nz * math.pi
+        theta_zi_next = i / Nz * math.pi
+        z = - 0.5 * ((Ca / 2) * (1 - math.cos(theta_zi)) + (Ca / 2) * (1 - math.cos(theta_zi_next)))
+        if x in d:
+            d[x].append((z, loading[i - 1][j - 1]))
+        else:
+            d[x] = [(z, loading[i - 1][j - 1])]
+        # coordinates.append((x, z))
+print(d)
+# dictionary x -> (z, load)
