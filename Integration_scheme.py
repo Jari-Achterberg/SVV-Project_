314f7ff8544cc8bb47ac63288880497e9f67c358
jvanthooft <julian99@hotmail.nl>
2020-02-19 11:04:52 +0100

    New update

--- Integration_scheme.py
+++ Integration_scheme.py
@@ -1,1 +1,17 @@
-#Jari is ...
+# Making the integration_scheme
+
+# Input variables
+x1 = 4
+x2 = 6
+y1 = 3
+y2 = 8
+
+# Calculating parameters for linear equation y = ax + b
+a = (y2-y1)/(x2-x1)
+b = y1-((y2-y1)/(x2-x1))*x1
+
+# Integration scheme for f(x)=ax+b
+intaxb = (a/2)*(x2**2 - x1**2) + b*(x2-x1)
+
+# Integration scheme for f(x)=(ax+b)*x
+intxaxb = (1/3)*a*(x2**3 - x1**3) + 0.5*b*(x2**2-x1**2)
