744ddc6414eea9b14f4d2e823ceb0bf588988a02
jvanthooft <julian99@hotmail.nl>
2020-02-20 13:50:50 +0100

    Merge branch 'master' of C:\Users\Julian\OneDrive\Documenten\BSA_3d_year\Simulation_Verification_Validation\SVV-Project_ with conflicts.

--- Integration_scheme.py
+++ Integration_scheme.py
@@ -1,6 +1,4 @@
 # Making the integration_scheme
-from Coordinates import d, Nx, Nz
-
 
 def integral(x1, x2, y1, y2):
     # Calculating parameters for linear equation y = ax + b
@@ -8,33 +6,25 @@ def integral(x1, x2, y1, y2):
     b = y1-((y2-y1)/(x2-x1))*x1
 
     # Integration scheme for f(x)=ax+b
-    intaxbvar = (a/2)*(x2**2 - x1**2) + b*(x2-x1)
-    # should be similar to: (y1 + y2)*(x2 - x1)/2
+    intaxb = (a/2)*(x2**2 - x1**2) + b*(x2-x1)
 
     # Integration scheme for f(x)=(ax+b)*x
-    intxaxbvar = (1/3)*a*(x2**3 - x1**3) + 0.5*b*(x2**2-x1**2)
+    intxaxb = (1/3)*a*(x2**3 - x1**3) + 0.5*b*(x2**2-x1**2)
 
-    return intaxbvar, intxaxbvar
+    return intaxb,intxaxb
 
+from Coordinates import d,Nx,Nz
 
-index = 0
-line_load, torques = [], []
+index = 0           # Variable only used for indexing
+int = []
+intx = []
 
-for i in range(0, Nx):
+for x in range(0,Nx):
 
-    load = 0        # kN / m (both variables are integrated once,
-    torque = 0      # kN      they become a 2D representation of the 3D distributed load)
+    for j in range(0,Nz-1):
 
-    for j in range(0, Nz-1):
+        intaxb, intxaxb = integral(d[index][1],d[index+1][1],d[index][2],d[index+1][2])
+        int.append()
 
-        intaxb, intxaxb = integral(d[index][1], d[index+1][1], float(d[index][2]), float(d[index+1][2]))
-        load += intaxb
-        torque += intxaxb
         index += 1
 
-    line_load.append(load)
-    torques.append(torque)
-
-# check if found loads and torques have reasonable numbers
-print(line_load[3])
-print(torques[3])
